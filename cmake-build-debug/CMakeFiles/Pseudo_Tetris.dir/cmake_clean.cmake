ba8dea588df3fd53c551a6c34dd7ae79ea1d2df8
Mathis Lécuyer <mathis.lecuyer.pro@gmail.com>
2021-11-02 11:49:22 +0100

    Creation des fichiers
    Creation des differents fichiers de travail

---
+++ cmake-build-debug/CMakeFiles/Pseudo_Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pseudo_Tetris.dir/main.c.o"
+  "CMakeFiles/Pseudo_Tetris.dir/tetris.c.o"
+  "Pseudo_Tetris.pdb"
+  "Pseudo_Tetris"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pseudo_Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Pseudo_Tetris.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pseudo_Tetris.
+# This may be replaced when dependencies are built.
