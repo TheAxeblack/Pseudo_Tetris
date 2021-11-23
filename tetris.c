79e8b2c7f7c4204749a8dc7bad413c5e7a29394d
Mathis Lécuyer <mathis.lecuyer.pro@gmail.com>
2021-11-23 16:54:25 +0100

    maj point de spawn

--- tetris.c
+++ tetris.c
@@ -130,7 +130,7 @@ void insert(puit p, forme f) {
     for (i = 0; i < f.width; i++) {
         for (j = 0; j < f.length; j++) {
             if (f.matrice[i][j] == 1)
-                p[f.width + i][(COLONNES / 2) - j] = f.matrice[i][j];
+                p[i][((COLONNES / 2) - 1) + j] = f.matrice[i][j];
         }
     }
 }
